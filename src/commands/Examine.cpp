d0a25837f9165c84001170812240e7a9e3b77868
unknown <greenej@.synergex.loc>
2012-04-18 15:41:51 -0700

    Implemented Examine command

--- src/commands/Examine.cpp
+++ src/commands/Examine.cpp
@@ -41,22 +41,61 @@ public:
     _symbolProvider = symbolProvider;
   }
   
+  void insertStructureFields(uint16_t address, string typeName, basic_ptree<string, string>& response)
+  {
+    if(!type)
+        type = "PTR";
+      
+    //command comes in the form of [address {type|WORD|DWORD|QWORD|PTR}]
+    auto resolvedStructure = _symbolProvider->ResolveStructure(type);
+    if(resolvedStructure != nullptr)
+    {
+      auto fields = resolvedStructure->AllFields();
+      for(auto field : fields)
+      {
+        auto targetName = name + "." + field->Name();
+        auto fieldAddress = address + field->OffsetFromParent();
+        rslt.put<string>(targetName + ".readable", field->ReadableValue(fieldAddress));
+      }
+    }
+  }
+  
   virtual basic_ptree<string, string> exec(basic_ptree<string, string>& commandPayload)
   {
     basic_ptree<string, string> rslt;
     
     auto address = commandPayload.get_optional<string>("address");
     auto name = commandPayload.get_optional<string>("name");
-    
+    auto type = commandPayload.get_optional<string>("type");
     if(address)
     {
-      //command comes in the form of [address {type|WORD|DWORD|QWORD|PTR}]
+      insertStructureFields(address, type, rslt);
     }
     else if(name)
     {
       //name can be a register
       //command comes in the form of [name {type|WORD|DWORD|QWORD|PTR}]
       //this must be a global field name or a label (if we want to view the disassembly)
+      auto foundFields = _symbolProvider->GlobalFields(name);
+      if(foundFields.size() == 1)
+      {
+        auto globalField = foundFields.front();
+        insertStructureFields(globalField->Address(), type ? type : globalField->Type()->Name(), rslt);
+      }
+      else if(foundFields.size() > 1)
+      {
+        rslt.put<string>("result", "failure");
+        rslt.put<string>("reason", "ambigious symbol");
+      }
+      else 
+      {
+        auto foundLabel = _symbolProvider->ResolveLabel(name);
+        if(foundLabel != nullptr)
+        {
+          //it looks like we want to disassemble the line that the label is from
+          //TODO: this isnt a primary feature do it later
+        }
+      }
     }
     else
     {
